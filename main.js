819b247271386fdf6cc72c78682acad96b9398aa
Boboshko-Ksenia <you@example.com>
2026-09-08 20:49:47 +0300

    Ksyusha

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ const express = require("express")
 
 const PORT = 8000
 const HOST = "localhost"
+const ENDPOINT = "/Ksyusha"
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.get(`${ENDPOINT}`, (req, res) => {
     res.status(200).json({
-        text: "Hello World!"
+        text: "Boboshko Ksyusha",
+        age: 15
     })
 })
 
 app.listen(PORT, HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}`)
+    console.log(`Server is running on http://${HOST}:${PORT}/${ENDPOINT}`)
 })
