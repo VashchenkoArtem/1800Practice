33fa3bf708083fbccc8affb1c2118a8ddf67aaec
Valentin5944 <valek.prt2013@gmail.com>
2026-09-08 20:39:32 +0300

    Practice

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const express = require("express")
+import express from "express"
 
 const PORT = 8000
 const HOST = "localhost"
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/Valentin', (req, res) => {
     res.status(200).json({
-        text: "Hello World!"
+        text: "Мене звуть Валентин, мені 13 років.Я живу в Україні.Зараз сижу,делаю енд поинт.Люблю грати в игри и иногда кодить самому, ну ето наверно понятно!"
     })
 })
 
 app.listen(PORT, HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}`)
+    console.log(`Server is running on http://${HOST}:${PORT}/Valentin`)
 })
