fbd4d42fb935948548f09e21c9693be9c5bce9f4
Artem <artemvaschenko83@gmail.com>
2026-09-08 20:23:05 +0300

    First commit. Init project

---
+++ main.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const PORT = 8000
+const HOST = "localhost"
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        text: "Hello World!"
+    })
+})
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`)
+})
