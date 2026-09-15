1e7529da8b3a58b374ae62d48f450f6368f8b2f7
Artem <artemvaschenko83@gmail.com>
2026-09-15 21:46:29 +0300

    Work with async. Post request

--- main.js
+++ main.js
@@ -87,9 +87,3 @@ app.listen(PORT, HOST, () => {
     console.log(`Server is running on http://${HOST}:${PORT}`)
 })
 
-
-app.get('/api', (req, res) => {
-    res.status(200).json({
-        name: "Maksym"
-    })
-})
