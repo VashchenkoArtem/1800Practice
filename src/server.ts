01aea291ef4bb164cb09cad1f7357d52fbcb374e
Artem <artemvaschenko83@gmail.com>
2026-09-26 11:39:13 +0300

    Types

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import { productsRouter } from "./transport/routers/products.js"
 
 const PORT = 8000
 const HOST = "localhost"
@@ -6,8 +7,10 @@ const HOST = "localhost"
 const app = express()
 
 app.use(express.json())
+app.use("/products", productsRouter)
 
 app.listen(PORT, HOST, () => {
     console.log(`Server is running on http://${HOST}:${PORT}`)
 })
 
+// DTO (Data Transfer Object)
--- src/transport/handlers/products.ts
+++ src/transport/handlers/products.ts
@@ -1,22 +1,34 @@
-import { getProducts, findProduct, createNewProduct } from "../services/products.js"
+import { getProducts, findProduct, createNewProduct } from "../../services/products.js"
+import type { ProductResponse } from "../dto/responses.js";
+import type { CreateProductRequest } from "../dto/requests.js";
+import type { ErrorResponse } from "../dto/errors.js";
+import type { Request, Response } from "express";
 
-export function getAllProducts(req, res){
 
+export function getAllProducts(
+    req: Request,
+    res: Response<ProductResponse[] | ErrorResponse>
+){
     const { take } = req.query
     if (!take) {
         return res.status(200).json(getProducts());
     }
-    const takeNum = parseInt(take)
+    const takeNum = Number(take)
 
     if(!takeNum || !Number.isInteger(takeNum) || takeNum < 0) {
-        res.status(400).json({ok: false, description: "Query parameter 'take' is incorrect!"})
+        res.status(400).json({
+            message: "Query parameter 'take' is incorrect!"
+        })
         return
     }
     const products =  getProducts(take)
-    return res.status(200).json({result: products})
+    return res.status(200).json(products)
 }
 
-export function getProductById(req, res) {
+export function getProductById(
+    req: Request, 
+    res: Response<ProductResponse | ErrorResponse>
+) {
     const productId = Number(req.params.id);
 
     if (!Number.isInteger(productId) || productId <= 0) {
@@ -36,7 +48,9 @@ export function getProductById(req, res) {
     return res.status(200).json(product);
 }
 
-export async function createProduct(req, res) {
+export async function createProduct(
+    req: Request<{}, {}, CreateProductRequest>, 
+    res: Response<ProductResponse | ErrorResponse>) {
     const { title, price, description } = req.body;
 
     if (
